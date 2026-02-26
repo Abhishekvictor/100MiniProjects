8b94d6d6f13a6ffec397795e5c036cd2d628bf79
Abhsishekvictor <abhishekvictor03@gmail.com>
2026-02-26 10:45:32 +0530

    Feat: create the color changing box and there Hex code

---
+++ RandomColorPaltte/random-color.js
@@ -0,0 +1,52 @@
+const randomcolor = function(){
+
+    
+    const hex = "0123456789ABCDEF"
+    let color = "#"
+    for(i=0; i<6; i++){
+        color+=hex[Math.floor(Math.random()*16)]
+    }
+    return color;
+}
+console.log(randomcolor());
+
+document.getElementById('color-btn').addEventListener('click', function(){
+
+    const box = document.getElementById('one-color')
+    box.style.backgroundColor=randomcolor()
+
+    box.innerText =randomcolor()
+
+})
+document.getElementById('color-btn').addEventListener('click', function(){
+
+    const box = document.getElementById('two-color')
+    box.style.backgroundColor=randomcolor()
+
+    box.innerText =randomcolor()
+
+})
+document.getElementById('color-btn').addEventListener('click', function(){
+
+    const box = document.getElementById('three-color')
+    box.style.backgroundColor=randomcolor()
+
+    box.innerText =randomcolor()
+
+})
+document.getElementById('color-btn').addEventListener('click', function(){
+
+    const box = document.getElementById('four-color')
+    box.style.backgroundColor=randomcolor()
+
+    box.innerText =randomcolor()
+
+})
+document.getElementById('color-btn').addEventListener('click', function(){
+
+    const box = document.getElementById('five-color')
+    box.style.backgroundColor=randomcolor()
+
+    box.innerText =randomcolor()
+
+})
